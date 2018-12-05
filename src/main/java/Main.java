274d303c4366e8171ccced79308a8435e4cf93a1
nguyenduylinhtv180 <nguyenduylinhtv180@gmail.com>
2018-12-05 17:55:05 +0700

    full commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,31 @@
+
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.servlets.DoSFilter;
 import org.glassfish.jersey.servlet.ServletContainer;
-
+import javax.servlet.DispatcherType;
+import java.util.EnumSet;
 /**
  *
- * @author dangg
+ * @author duylinh
  */
 public class Main {
     public static void main(String[] args) throws Exception {
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
+        final EnumSet<DispatcherType> REQUEST_SCOPE = EnumSet.of(DispatcherType.REQUEST);
+
+        context.addFilter( RequestLoggingFilter.class, "/*", REQUEST_SCOPE );
+
+        FilterHolder filterHolder = new FilterHolder( DoSFilter.class );
+               filterHolder.setInitParameter("maxRequestsPerSec", "1");  // max requests per second per client
+             filterHolder.setInitParameter("delayMs", "-1");           // millisec to delay excess requests. -1 means reject (for testing)
+              filterHolder.setInitParameter("remotePort", "false");     // true = track connections by remote ip+port
+             filterHolder.setInitParameter("enabled", "true");
+              filterHolder.setInitParameter("trackSessions", "true");
+              context.addFilter( filterHolder, "/*", REQUEST_SCOPE );
 
         String port = System.getenv("PORT") != null ? System.getenv("PORT") : "8080";
 
