c45eb6395a277198dac93b69d48fdb7a9bf4ca1e
nguyenduylinhtv180 <nguyenduylinhtv180@gmail.com>
2018-12-06 09:51:59 +0700

    full ---version

--- src/main/java/Information.java
+++ src/main/java/Information.java
@@ -10,13 +10,25 @@ public class Information {
     private long lastupdated_date;
     private String lastupdated_by;
 
-
+    static int id_name;
     public Information() {
+        id_name++;
+        this.id=id_name+"";
+        this.created_date = new Date().getTime();
 
     }
+    public Information(String content, String created_by) {
+        id_name ++;
+        this.id = id_name + "";
+        this.content = content;
+        this.created_by = created_by;
+        this.created_date = new Date().getTime();
+        System.out.println(this.created_date);
+    }
 
-    public Information(String id, String content, long created_date,String created_by,long lastupdated_date, String lastupdated_by) {
-        this.id = id;
+    public Information(String content, long created_date,String created_by,long lastupdated_date, String lastupdated_by) {
+        id_name ++;
+        this.id = id_name + "";
         this.content = content;
         this.created_date = created_date;
         this.created_by =created_by;
